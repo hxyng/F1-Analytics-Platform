8d73ad5810bb488ead7b0698e5c5946c8529ea50
hxyvng <hxyvng@gmail.com>
2025-10-04 14:51:18 -0500

    changes

--- fastf1-service/app.py
+++ fastf1-service/app.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import fastf1
 
 app = FastAPI()
+
+origins = [
+        "http://localhost:3000"
+]
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,  # Allow cookies, authorization headers, etc.
+        allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+        allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/lap-times/{year}/{gp}/{driver}")
 def get_lap_times(year: int, gp: str, driver: str):
